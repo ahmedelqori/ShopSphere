3c185bd06cd7b6cc3e9569c7bb911dc77bc8dbb7
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2024-11-26 21:52:03 +0100

    Fix Shooping list

--- client/src/components/Header/Main/ShoopingCart/ShoopingCart.styled.tsx
+++ client/src/components/Header/Main/ShoopingCart/ShoopingCart.styled.tsx
@@ -3,11 +3,10 @@ import styled from "styled-components";
 export const Cart = styled.div`
   gap: 1rem;
   top: 100%;
-  right: 0;
+  right: 40px;
   width: 376px;
   font-size: 0.9rem;
   position: absolute;
-  padding: 10px 20px;
   align-items: start;
   border-radius: 2px;
   flex-direction: column;
@@ -19,12 +18,14 @@ export const Cart = styled.div`
 export const H3 = styled.h3`
   font-weight: 600;
   font-size: 0.9rem;
+  padding: 10px 20px;
 `;
 
 export const Ul = styled.ul`
   display: flex;
   gap: 15px;
   flex-direction: column;
+  padding: 10px 20px;
 `;
 
 export const Li = styled.li`
@@ -59,4 +60,49 @@ export const EmptyList = styled.div`
   font-size: 1.5rem;
   padding: 10px 0;
   align-self: center;
+`;
+
+export const ShowMore = styled.div`
+  align-self: center;
+  font-size: 1.2rem;
+  cursor: pointer;
+  color: var(--color-grey-light);
+  font-weight: 500;
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: var(--color-grey);
+  opacity: 0.4;
+  margin: 5px 0;
+`;
+
+export const Total = styled.div`
+  padding: 10px 20px;
+  justify-content: space-between;
+  div {
+    font-size: 0.9rem;
+    font-weight: 500;
+  }
+`;
+
+export const Buttons = styled.div`
+  padding: 10px 20px;
+  gap: 10px;
+  flex-direction: column;
+  button {
+    width: 100%;
+    padding: 10px;
+    font-weight: 600;
+    font-size: 0.9rem;
+    color: var(--color-white);
+    border: none;
+    background-color: var(--color-orange);
+    &:last-child {
+      color: var(--color-orange);
+      border: 1px solid var(--color-orange);
+      background-color: var(--color-white);
+    }
+  }
 `;
--- client/src/components/Header/Main/ShoopingCart/ShoopingCart.tsx
+++ client/src/components/Header/Main/ShoopingCart/ShoopingCart.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Cart,
   H3,
@@ -9,6 +9,10 @@ import {
   InfoContainer,
   PriceContainer,
   EmptyList,
+  ShowMore,
+  Line,
+  Total,
+  Buttons,
 } from "./ShoopingCart.styled";
 
 type ShoppingCartProps = {
@@ -30,42 +34,71 @@ const ShoppingCart: React.FC<ShoppingCartProps> = ({ style }) => {
     img: "/assets/icons/Item.png",
   };
 
+  const itemObj1: ItemInterface = {
+    title: "Canon EOS 1500D DSLR Camera Body+ 18-55 mm",
+    price: 1500,
+    count: 5,
+    img: "/assets/icons/Item.png",
+  };
   const [items, setItems] = useState<ItemInterface[]>([
+    itemObj,
+    itemObj1,
     itemObj,
     itemObj,
     itemObj,
   ]);
-
+  const [total, setTotal] = useState<number>(0);
   const handleRemove = (index: number): void => {
     setItems((prevItems) => prevItems.filter((_, i) => i !== index));
   };
 
+  useEffect(() => {
+    const newTotal = items.reduce(
+      (acc, item) => acc + item.price * item.count,
+      0
+    );
+    setTotal(newTotal);
+  }, [items]);
   return (
     <Cart style={style}>
       <H3>Shopping Cart: ({items.length})</H3>
+      <Line />
       <Ul>
         {items.length ? (
-          items.map((item: ItemInterface, index: number) => (
-            <Li key={index}>
-              <ItemImage className="item" src={item.img} alt="item" />
-              <InfoContainer>
-                <H4>{item.title}</H4>
-                <PriceContainer>
-                  <p>{item.count} x </p>
-                  <span>${item.price}</span>
-                </PriceContainer>
-              </InfoContainer>
-              <img
-                src="/assets/icons/X.svg"
-                alt="close"
-                onClick={() => handleRemove(index)}
-              />
-            </Li>
-          ))
+          items.map((item: ItemInterface, index: number) => {
+            if (index > 2) return;
+            return (
+              <Li key={index}>
+                <ItemImage className="item" src={item.img} alt="item" />
+                <InfoContainer>
+                  <H4>{item.title}</H4>
+                  <PriceContainer>
+                    <p>{item.count} x </p>
+                    <span>${item.price}</span>
+                  </PriceContainer>
+                </InfoContainer>
+                <img
+                  src="/assets/icons/X.svg"
+                  alt="close"
+                  onClick={() => handleRemove(index)}
+                />
+              </Li>
+            );
+          })
         ) : (
           <EmptyList>Empty List</EmptyList>
         )}
+        {items.length > 2 ? <ShowMore>Show More</ShowMore> : ""}
       </Ul>
+      <Line />
+      <Total>
+        <div>Sub-Total:</div>
+        <span>${total.toFixed(2)} USD</span>
+      </Total>
+      <Buttons>
+        <button>CHECKOUT NOT</button>
+        <button>VIEW CART</button>
+      </Buttons>
     </Cart>
   );
 };
